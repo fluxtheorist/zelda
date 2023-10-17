4c6c0bc2f9a964dcbcbf6a521bc8f2a753153871
fluxtheorist <jashdesai27@gmail.com>
2023-10-17 00:26:34 -0400

    display comment

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class Game:
         self.level = Level()
 
     def run(self):
+        # running display
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
